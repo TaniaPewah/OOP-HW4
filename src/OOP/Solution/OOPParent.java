9e065064bcd981b557dad937bd5eca488846dfa9
inbal-tz <42312218+inbal-tz@users.noreply.github.com>
2020-01-13 01:05:09 +0200

    target fixed. need to fix protected

--- src/OOP/Solution/OOPParent.java
+++ src/OOP/Solution/OOPParent.java
@@ -3,7 +3,10 @@ package OOP.Solution;
 import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
 
+@Target(ElementType.TYPE)
 @Repeatable(OOPParents.class)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface OOPParent {
--- src/OOP/Solution/OOPParents.java
+++ src/OOP/Solution/OOPParents.java
@@ -1,7 +1,10 @@
 package OOP.Solution;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface OOPParents {
     // This is the container annotation for OOPParent.
